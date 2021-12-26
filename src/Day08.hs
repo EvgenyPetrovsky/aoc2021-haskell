5deec34e7dfc8445856641706df6526c8efdede0
Evgeny Petrovsky <evgeny.petrovsky@ing.com>
2021-12-26 10:23:45 +0100

    finish day 8

--- src/Day08.hs
+++ src/Day08.hs
@@ -66,30 +66,31 @@ determine :: [Signal] -> Determinant
 determine signals =
   M.fromList $ zip [d0, d1, d2, d3, d4, d5, d6, d7, d8, d9] [0..]
   where
+    first = S.elemAt 0
     s_0123456789 :: S.Set (S.Set Char)
     s_0123456789 = S.fromList signals
-    d1 = S.elemAt 1 . S.filter (\x -> S.size x == 2) $ s_0123456789
-    d7 = S.elemAt 1 . S.filter (\x -> S.size x == 3) $ s_0123456789
-    d4 = S.elemAt 1 . S.filter (\x -> S.size x == 4) $ s_0123456789
-    d8 = S.elemAt 1 . S.filter (\x -> S.size x == 7) $ s_0123456789
+    d1 = first . S.filter (\x -> S.size x == 2) $ s_0123456789
+    d7 = first . S.filter (\x -> S.size x == 3) $ s_0123456789
+    d4 = first . S.filter (\x -> S.size x == 4) $ s_0123456789
+    d8 = first . S.filter (\x -> S.size x == 7) $ s_0123456789
     s_023569 = foldl (flip S.delete) s_0123456789 [d1,d4,d7,d8]
     -- digit 3 has 5 segments and is superset of digit 7
-    d3 = S.elemAt 1 . S.filter (\x -> S.intersection x d7 == d7 && S.size x == 5) $ s_023569
+    d3 = first . S.filter (\x -> S.intersection x d7 == d7 && S.size x == 5) $ s_023569
     s_02569 = S.delete d3 s_023569
     -- digit 9 has 6 segments and is superset of digit 3
-    d9 = S.elemAt 1 . S.filter(\x -> S.intersection x d3 == d3 && S.size x == 6) $ s_02569
+    d9 = first . S.filter(\x -> S.intersection x d3 == d3 && S.size x == 6) $ s_02569
     s_0256 = S.delete d9 s_02569
     -- digit 0 has 6 segments and is superset of 7
-    d0 = S.elemAt 1 . S.filter(\x -> S.intersection x d7 == d7 && S.size x == 6) $ s_0256
+    d0 = first . S.filter(\x -> S.intersection x d7 == d7 && S.size x == 6) $ s_0256
     s_256 = S.delete d0 s_0256
     -- digit 6 is the only remaining digit with 6 segments
-    d6 = S.elemAt 1 . S.filter(\x -> S.size x == 6) $ s_256
+    d6 = first . S.filter(\x -> S.size x == 6) $ s_256
     s_25 = S.delete d6 s_256
     -- digit 5 has 5 segments and is subset of 6
-    d5 = S.elemAt 1 . S.filter(\x -> S.intersection x d6 == x && S.size x == 5) $ s_25
+    d5 = first . S.filter(\x -> S.intersection x d6 == x && S.size x == 5) $ s_25
     s_2 = S.delete d5 s_25
     -- digit 2 is the only remaining digit
-    d2 = S.elemAt 1 s_2
+    d2 = first s_2
 
 decode :: [Signal] -> Determinant -> Int
 decode ss d =
